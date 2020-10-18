25781dc83742485736836b55472a599eb19206fc
kyonRay <rui.kwok@outlook.com>
2020-10-19 00:19:42 +0800

    add java leetcode.

---
+++ leetcode/java/esay/1.两数之和.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target-nums[i])){
+                return new int[]{i,map.get(target-nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1,-1};
+    }
+}
+// @lc code=end
+
